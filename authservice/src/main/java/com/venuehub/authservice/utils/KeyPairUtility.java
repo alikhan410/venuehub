ee43c7b70dc12954fdcc0ec6e2b75acf2bb6a717
alikhan410 <mczeyo@gmail.com>
2024-07-03 05:59:06 +0500

    changed RSAKeyProperties

--- authservice/src/main/java/com/venuehub/authservice/utils/KeyPairUtility.java
+++ authservice/src/main/java/com/venuehub/authservice/utils/KeyPairUtility.java
@@ -2,6 +2,7 @@ package com.venuehub.authservice.utils;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.util.Base64;
 
 public class KeyPairUtility {
 
@@ -11,6 +12,11 @@ public class KeyPairUtility {
             KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
             keyPairGenerator.initialize(2048);
             keyPair = keyPairGenerator.generateKeyPair();
+            String publicKeyStr = Base64.getEncoder().encodeToString(keyPair.getPublic().getEncoded());
+            String privateKeyStr = Base64.getEncoder().encodeToString(keyPair.getPrivate().getEncoded());
+
+            System.out.println("Public Key: " + publicKeyStr);
+            System.out.println("Private Key: " + privateKeyStr);
         }
         catch (Exception ex) {
             throw new IllegalStateException(ex);
--- authservice/src/main/java/com/venuehub/authservice/utils/RSAKeyProperties.java
+++ authservice/src/main/java/com/venuehub/authservice/utils/RSAKeyProperties.java
@@ -1,11 +1,16 @@
 package com.venuehub.authservice.utils;
 
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 @Component
 @Getter
@@ -13,9 +18,25 @@ public class RSAKeyProperties {
     private final RSAPublicKey publicKey;
     private final RSAPrivateKey privateKey;
 
-    public RSAKeyProperties() {
-        KeyPair keyPair = KeyPairUtility.generate();
-        this.publicKey = (RSAPublicKey) keyPair.getPublic();
-        this.privateKey = (RSAPrivateKey) keyPair.getPrivate();
+    //Use this if you want to create a random key on each startup
+//    public RSAKeyProperties() {
+//        KeyPair keyPair = KeyPairUtility.generate();
+//        this.publicKey = (RSAPublicKey) keyPair.getPublic();
+//        this.privateKey = (RSAPrivateKey) keyPair.getPrivate();
+//    }
+
+
+    public RSAKeyProperties(@Value("${jwt.publicKey}") String publicKeyStr,
+                            @Value("${jwt.privateKey}") String privateKeyStr) throws Exception {
+        byte[] publicKeyBytes = Base64.getDecoder().decode(publicKeyStr);
+        byte[] privateKeyBytes = Base64.getDecoder().decode(privateKeyStr);
+
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
+        PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+
+        this.publicKey = (RSAPublicKey) keyFactory.generatePublic(publicKeySpec);
+        this.privateKey = (RSAPrivateKey) keyFactory.generatePrivate(privateKeySpec);
     }
+
 }
